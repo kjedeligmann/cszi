c93a8253c72bd4ec7fb38c92b77d6df58f40bfd5
kjedeligmann <dlyauchebyacc@gmail.com>
2024-07-15 18:12:34 +0300

    Fmt lfsr.go

--- lfsr/lfsr.go
+++ lfsr/lfsr.go
@@ -7,57 +7,57 @@ import (
 )
 
 func LFSR(start uint64, polynomial uint64, filename string) {
-    read, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer read.Close()
+	read, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer read.Close()
 
-    write, err := os.Create(filename + ".lfsr")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer write.Close()
+	write, err := os.Create(filename + ".lfsr")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer write.Close()
 
-    lfsr := start
-    var order uint8
+	lfsr := start
+	var order uint8
 
-    for taps := polynomial; taps != 0; taps >>= 1 {
-        order++
-    }
-    polynomial ^= (1 << (order-1))
+	for taps := polynomial; taps != 0; taps >>= 1 {
+		order++
+	}
+	polynomial ^= (1 << (order - 1))
 
-    reader := bufio.NewReader(read)
-    writer := bufio.NewWriter(write)
+	reader := bufio.NewReader(read)
+	writer := bufio.NewWriter(write)
 
-    for {
-        b, err := reader.ReadByte()
-        if err != nil {
-           break
-        }
-        g := LFSRbyte(&lfsr, &polynomial, &order)
-        writer.WriteByte(b^g)
-    }
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			break
+		}
+		g := LFSRbyte(&lfsr, &polynomial, &order)
+		writer.WriteByte(b ^ g)
+	}
 
-    writer.Flush()
+	writer.Flush()
 }
 
 func LFSRbyte(register *uint64, polynomial *uint64, order *uint8) (result byte) {
-    for i := 7; i >= 0; i-- {
-        result |= byte((*register & 1) << i)
-        var bit uint64
-        bits := *register & *polynomial
-        for ; bits != 0; bits >>= 1 {
-            bit ^= bits
-        }
-        bit &= 1
-        *register = (*register >> 1) | (bit << (*order-1))
-    }
-    return
+	for i := 7; i >= 0; i-- {
+		result |= byte((*register & 1) << i)
+		var bit uint64
+		bits := *register & *polynomial
+		for ; bits != 0; bits >>= 1 {
+			bit ^= bits
+		}
+		bit &= 1
+		*register = (*register >> 1) | (bit << (*order - 1))
+	}
+	return
 }
 
 func main() {
-    var start, polynomial uint64 = 0xACE1, 0b1000000000101101
-    LFSR(start, polynomial, "hello.txt")
-    LFSR(start, polynomial, "hello.txt.lfsr")
+	var start, polynomial uint64 = 0xACE1, 0b1000000000101101
+	LFSR(start, polynomial, "hello.txt")
+	LFSR(start, polynomial, "hello.txt.lfsr")
 }
